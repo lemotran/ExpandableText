d2444da9fcf67f5f095d3a7b587555c681942da4
Lemo Tran <tranduchieu@me.com>
2025-02-12 14:34:33 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ExpandableText",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v17),
+        .macOS(.v14),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
